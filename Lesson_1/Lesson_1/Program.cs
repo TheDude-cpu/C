a34fe4c4798b244b94ab34ab42f330040129278d
DJ <Dell@DESKTOP-BAQH81L>
2020-02-23 15:36:22 +0100

    AddLastMethod

--- Lesson_1/Lesson_1/Program.cs
+++ Lesson_1/Lesson_1/Program.cs
@@ -6,42 +6,85 @@ namespace Hello_200212
     {
         static void Main(string[] args)
         {
-            for (; ; )
+            ValueTypes();
+
+            for (; ; ) // loop infinit
             {
-                string name; //= args[0];
-                Console.Write("Wpisz swoje imie: ");
-                //samo write powoduje ze pytanie i odp beda w tej samej linii
-                name = Console.ReadLine();
-                Console.WriteLine("Hello" + " " + name);
+                Greeting();
 
-                Console.Write("Wpisz ile masz lat: ");
+                Age();
 
-                int age;
-                bool result = int.TryParse(Console.ReadLine(), out age); // change string to ing 
-                                                                         //int age = int.Parse(Console.ReadLine()); // change string to ing 
-
-                if (age > 18)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green; // zmiana koloru tekstu
-                    Console.WriteLine("Brawo jesteś pełnoletni, możesz wypić browara!");
-                }
-                else if (result == false)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red; // zmiana koloru tekstu
-                    Console.WriteLine("wprowadziłeś niepoprawny wiek!");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue; // zmiana koloru tekstu
-                    Console.WriteLine("możemy Ci zaoferować mleko");
-                }
-
-                Console.ReadKey();  // powstrzymywanie konsoli przed zamknięciem :)
-                
-                Console.ResetColor(); // resetowanie koloru
+                Settings();
             }
         }
 
+        /// <summary>
+        /// min and max int value
+        /// </summary>
+        private static void ValueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+
+            Console.WriteLine("MaxInt =" + maxInt);
+            Console.WriteLine("MinInt =" + minInt);
+            Console.WriteLine("MaxLong =" + maxLong);
+            Console.WriteLine("MinLong =" + minLong);
+
+
+        }
+
+        /// <summary>
+        /// settings
+        /// </summary>
+        private static void Settings()
+        {
+            Console.ReadKey();  // powstrzymywanie konsoli przed zamknięciem :)
+            Console.Clear();
+            Console.ResetColor(); // resetowanie koloru
+        }
+
+        /// <summary>
+        /// check user's age
+        /// </summary>
+        private static void Age()
+        {
+            Console.Write("Wpisz ile masz lat: ");
+
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age); // change string to ing 
+                                                                     //int age = int.Parse(Console.ReadLine()); // change string to ing 
+
+            if (age > 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green; // zmiana koloru tekstu
+                Console.WriteLine("Brawo jesteś pełnoletni, możesz wypić browara!");
+            }
+            else if (result == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red; // zmiana koloru tekstu
+                Console.WriteLine("wprowadziłeś niepoprawny wiek!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue; // zmiana koloru tekstu
+                Console.WriteLine("możemy Ci zaoferować mleko");
+            }
+        }
+
+        /// <summary>
+        /// write user's greeting
+        /// </summary>
+        private static void Greeting()
+        {
+            string name; //= args[0];
+            Console.Write("Wpisz swoje imie: ");
+            //samo write powoduje ze pytanie i odp beda w tej samej linii
+            name = Console.ReadLine();
+            Console.WriteLine("Hello" + " " + name);
+        }
     }
 
 }
