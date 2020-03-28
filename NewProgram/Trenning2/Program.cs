7f31d59d1df3e3a5cb7b4335924f33c4ed77d55f
DJ <Dell@DESKTOP-BAQH81L>
2020-03-28 17:02:01 +0100

    CreatRecatngle3

--- NewProgram/Trenning2/Program.cs
+++ NewProgram/Trenning2/Program.cs
@@ -10,6 +10,28 @@ namespace Trenning2
     {
         static void Main(string[] args)
         {
+            // wyswietlanie w konsoli prostokatu o szerokosci x oraz wysokoci y
+            // x oraz y podaje uzytkownikowi, prostokat zbudowany jest z "*"
+            Console.WriteLine("Podaj szerokosc:");
+            string xs = Console.ReadLine();
+            //Console.WriteLine("Podana szerokosc to" +" " + xs);
+            Console.WriteLine("Podaj wysokosc:");
+            string ys = Console.ReadLine();
+            //Console.WriteLine("Podana szerokosc to" +" " + ys);
+
+            int x = int.Parse(xs); // convert string to ing
+            int y = int.Parse(ys);
+
+            for (int i = 0; i<y; i++) // or i <= y-1 or i<y
+            {
+                for (int j = 0; j <x; j++)
+                {
+                    Console.Write("*");  // obok siebie
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
