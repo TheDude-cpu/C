fcc53802b813c9978eecd1253c25c664f8ecb10e
DJ <Dell@DESKTOP-BAQH81L>
2020-03-28 17:57:18 +0100

    RandomNumer

--- NewProgram/Program3/Program3/Program3/Program.cs
+++ NewProgram/Program3/Program3/Program3/Program.cs
@@ -10,6 +10,36 @@ namespace Program3
     {
         static void Main(string[] args)
         {
+            // pseudo losowosc w c#
+
+            //class
+            Random rnd = new Random();  // klasa rnd przyjmuje nowy obiekt klasy Random
+            int los = rnd.Next(1,11);
+            int myNumber;
+
+            Console.WriteLine("zgadnij liczbe z zakresu 1-10");
+
+            do
+            {
+                Console.Write("Podaj liczbe: ");
+                myNumber = int.Parse(Console.ReadLine());
+                if(los < myNumber)
+                {
+                    Console.WriteLine("Podana liczba jest wieksza niz wylosowana. Sproboj ponownie");
+                }
+                else if (los == myNumber)
+                {
+                    Console.WriteLine("Odgadles wylosowna liczbe");
+                }
+                else
+                {
+                    Console.WriteLine("Podana liczba jest mniejsza niz wylosowana. Sproboj ponownie");
+                }
+            } while (los != myNumber);
+
+
+
+            Console.ReadKey();
         }
     }
 }
